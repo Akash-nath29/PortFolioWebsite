9c17f7cbe0d090f1d99e4bdde25c471192a89d9f
Akash-nath29 <anath5440@gmail.com>
2024-02-28 01:58:53 +0530

    Add Certificate Section

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ window.onscroll = () => {
 //collapsed navbar for mobile window
 
 let menuList = document.getElementById("menuList");
-menuList.style.maxHeight = "0px" ;
+menuList.style.maxHeight = "0px";
 
 let menuBtn = document.getElementById("openMenuBtn");
 
@@ -37,7 +37,7 @@ let crossIcon = '<svg id="crossIcon" xmlns="http://www.w3.org/2000/svg" height="
 let menuIcon = '<svg id="menuIcon" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512" onclick="toggleMenu()"><style>#menuIcon{fill:#ffffff; margin-right: 2vh;}</style><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>';
 
 function toggleMenu() {
-    if(menuList.style.maxHeight == "0px") {
+    if (menuList.style.maxHeight == "0px") {
         menuList.style.maxHeight = "300px";
         menuBtn.innerHTML = crossIcon;
         menuBtn.style.display = "block";
@@ -52,16 +52,48 @@ function toggleMenu() {
 
 //sending Email
 
-function sendMail(){
+function sendMail() {
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "anath5440@gmail.com",
-        Password : "FF849425973D9801865E72568398DF0F985B",
-        To : 'akashnathkrishnagar@gmail.com',
-        From : document.getElementById("emailInput").value,
-        Subject : "User Enquery",
-        Body : document.getElementById("nameInput").value + " says: <br/>" + document.getElementById("textarea").value
+        Host: "smtp.elasticemail.com",
+        Username: "anath5440@gmail.com",
+        Password: "FF849425973D9801865E72568398DF0F985B",
+        To: 'akashnathkrishnagar@gmail.com',
+        From: document.getElementById("emailInput").value,
+        Subject: "User Enquery",
+        Body: document.getElementById("nameInput").value + " says: <br/>" + document.getElementById("textarea").value
     }).then(
-      message => alert("Message Sent Succesfully")
+        message => alert("Message Sent Succesfully")
     );
 }
+
+// SlideShow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+// Automatic scrolling every 3 seconds
+setInterval(function () {
+    plusSlides(1);
+}, 3000);
